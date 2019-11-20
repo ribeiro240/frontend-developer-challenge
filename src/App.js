b5b61e77f7c8e41dcc09e22cad94c9b701ff0c6e
Ribeiro <filipepaiva240@gmail.com>
2019-11-20 18:24:35 -0300

    Finishes header and footer

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <React.Fragment>
+      <header className="header">
+        <div className="header-content">
+          <div className="header-text">
+            <p className="header-text-top">uma seleção de produtos</p>
+            <p className="header-text-middle">especial para você</p>
+            <p className="header-text-bottom">Todos os produtos desta lista foram selecionados a partir da sua navegação. Aproveite!</p>
+          </div>
+          <nav className="navbar">
+            <button className="nav-item">Conheça a Linx</button>
+            <button className="nav-item">Ajude o algoritmo</button>
+            <button className="nav-item">Seus produtos</button>
+            <button className="nav-item">Compartilhe</button>
+          </nav>
+        </div>
+
       </header>
-    </div>
+      <main className="main">
+        <section className="product-gallery">
+
+        </section>
+      </main>
+      <footer className="footer">
+        <div className="footer-text">
+          <p>Testando suas habilidades em HTML, CSS e JS.</p>
+          <p>Linx Impulse</p>
+          <p>2019</p>
+        </div>
+
+      </footer>
+    </React.Fragment>
   );
 }
 
